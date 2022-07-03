7bf389bff1b94b3f8f5a044829fc88a68619d3b8
Max Kottong <max.kottong@gmail.com>
2022-07-03 16:21:40 -0400

    Added array for generating timeblocks, going to swap from timeblocks put in beforehand to dynamically

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,30 @@
 var currentDay = moment().format('dddd, MMMM Do');
 
+var timeBlocks = [
+    { hour: "9AM", time: 9 },
+    { hour: "10AM", time: 10 },
+    { hour: "11AM", time: 11 },
+    { hour: "12PM", time: 12 },
+    { hour: "1PM", time: 13 },
+    { hour: "2PM", time: 14 },
+    { hour: "3PM", time: 15 },
+    { hour: "4PM", time: 16 },
+    { hour: "5PM", time: 17 },
+];
+
 $(document).ready(function () {
     $("#currentDay").append(currentDay);
+})
+
+$(".saveBtn").click(function (event) {
+    event.preventDefault();
+    console.log("Clicked");
+})
+
+function generateTimeBlocks() {
+    $(".container").empty();
+}
+
+setInterval(function () {
+
 })
