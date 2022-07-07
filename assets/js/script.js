82418f6432153ff1d35d97a4b7ce97aa1883bd6b
Max Kottong <max.kottong@gmail.com>
2022-07-07 17:04:51 -0400

    Revert "Fixed problems with quotes on certain elements and added parentheses to generateTimeBlocks;"
    This reverts commit ea5c2c893bcbc0a6f5ee9fe8a709de64f2434c51.

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,20 +9,28 @@ var timeBlocks = [
     { hour: "2PM", time: 14 },
     { hour: "3PM", time: 15 },
     { hour: "4PM", time: 16 },
-    { hour: "5PM", time: 17 }
+    { hour: "5PM", time: 17 },
 ];
 
 var tasks = ["", "", "", "", "", "", "", "", ""];
 
-function start() {
-    var storedTasks = JSON.parse(localStorage.getItem("tasks"));
+$(document).ready(function () {
+    start();
+    
+    $("#currentDay").append(currentDay);
 
-    if (storedTasks !== null) {
-        tasks = storedTasks;
-    }
+    $(".saveBtn").click(function (event) {
+        event.preventDefault();
+        console.log("Clicked save button");
 
-    generateTimeBlocks();
-}
+        var dataIndex = $(this).attr("data-index");
+        var textInput = $("#input${dataIndex}").val();
+
+        tasks.splice(dataIndex, 1, textInput);
+
+        storeTasks();
+    })
+})
 
 function generateTimeBlocks() {
     $(".container").empty();
@@ -46,8 +54,8 @@ function generateTimeBlocks() {
 
         var timeBlockEl = $("<form>").attr("class", "input-group row");
         var hourDivEl = $("<div>").attr("class", "col-1");
-        var hourEl = $("<div>").attr("class", "hour text-right").text(hourName);
-        var inputEl = $("<textarea>").attr("class", `form-control textarea ${inputElStyle}`).attr("type", "text").attr("id", "input" + i).val(taskText);
+        var hourEl = $("<div>").attr("class", "hour text-right");
+        var inputEl = $("<textarea>").attr("class", "form-control textarea ${inputElStyle}").attr("type", "text").attr("id", "input" + i).val(taskText);
         var buttonEl = $("<div>").attr("class", "input-group-append");
         var button = $("<button>").attr("class", "saveBtn").attr("data-index", i);
         var saveIconEl = $("<span>").attr("class", "oi oi-hard-drive");
@@ -61,29 +69,10 @@ function generateTimeBlocks() {
     }
 }
 
-$(document).ready(function () {
-    start();
-    
-    $("#currentDay").append(currentDay);
-
-    $(".saveBtn").click(function (event) {
-        event.preventDefault();
-        console.log("Clicked save button");
-
-        var dataIndex = $(this).attr("data-index");
-        var textInput = $(`#input${dataIndex}`).val();
-
-        tasks.splice(dataIndex, 1, textInput);
-
-        storeTasks();
-    });
-
-    setInterval(function () {
-        currentHour = parseInt(moment().format("HH"));
-        generateTimeBlocks();
-    }, (1000 * 60) * 5);
-})
-
-function storeTasks() {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
+function start() {
+   
 }
+
+setInterval(function () {
+
+})
